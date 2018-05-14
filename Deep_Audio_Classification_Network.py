e049e569eed8a62e39ed7aade9bffbcca1b9abcd
Tim Rault <tim.rault@gmail.com>
2018-05-14 11:23:02 +0200

    Add comments to functions

--- Deep_Audio_Classification_Network.py
+++ Deep_Audio_Classification_Network.py
@@ -29,7 +29,7 @@ dst = 'gtzan_music_speech'
 if not os.path.exists(dst):
     dataset_utils.gtzan_music_speech_download(dst)
 
-
+print('==== Preparing audio files for training... ====')
 # Let's get the list of all the music and speech wave files
 # Get the full path to the music directory
 music_dir = os.path.join(os.path.join(dst, 'music_speech'), 'music_wav')
@@ -61,24 +61,31 @@ n_frames = n_frames_per_second // 2
 frame_hops = n_frames_per_second // 4
 
 
-def prepare_audio_file(wav_file, label=None, fft_size=512, hop_size=256):
+def prepare_audio_file(wav_file, label=None, fft_size=512, hop_size=256, n_frames=43, frame_hops=21):
     """
-    Prepares and audio file by taking the Discrete Fourier Transform of it
-    over many sliding windows, and returns the corresponding magnitudes
-    and labels
-    :param wav_file: Path to the .wav file to prepare
-    :param label: The label associated to the .wav if it is a labeled file (music: 0, speech: 1)
-    :param fft_size: Length of each window in the DFT
-    :param hop_size: Sliding length
-    :return: magnitudes array containing the magnitudes corresponding to the sliding windows,
+    Prepares and audio file by splitting it into frames of length fft_size,
+    and by taking their Discrete Fourier Transforms. Frames are then grouped
+    together in batches of n_frames (corresponding to 500ms of audio), sliding
+    the window by 250ms (frame_hops) at a time. The label corresponding to
+    each batch (music: 0 or speech: 1) is kept in the labels variable.
+    :param wav_file: path to the .wav file to prepare
+    :param label: the label associated to the .wav, if it is a labeled file (music: 0, speech: 1)
+    :param fft_size: length of each frame to split the input and apply the DFT
+    :param hop_size: by how many points we move every time we extract a frame from the input
+    :param n_frames: length of the sliding window (i.e. the number of frames to take to
+    make a batch of 500ms of audio)
+    :param frame_hops: by how many ms we move our sliding window over the frames list every
+    time we make a new 500ms batch
+    :return: magnitudes array containing the magnitudes corresponding to the sliding windows
+    (i.e. arrays containing 500ms worth of audio in the form of multiple frames of magnitudes),
     and labels array containing the labels corresponding to each sliding window.
     """
     s = utils.load_audio(wav_file)
-    # Take the Discrete Fourier Transform:
+    # Split into frames and take their Discrete Fourier Transforms:
     re, im = dft.dft_np(s, fft_size=fft_size, hop_size=hop_size)
-    # And convert the complex representation (cartesian) to magnitudes/phases (polar representation):
+    # Convert the complex representation (cartesian) to magnitudes/phases (polar representation):
     mag, phs = dft.ztoc(re, im)
-    # This is how many sliding windows we have:
+    # This is how many sliding windows we have
     n_hops = (len(mag) - n_frames) // frame_hops
     # Let's extract them all:
     magnitudes, labels = [], []
@@ -87,43 +94,48 @@ def prepare_audio_file(wav_file, label=None, fft_size=512, hop_size=256):
         frames = mag[(hop_i * frame_hops):(hop_i * frame_hops + n_frames)]
         # We'll take the log magnitudes, as this is a nicer representation:
         this_X = np.log(np.abs(frames[..., np.newaxis]) + 1e-10)
+        # Append this group of frames (corresponding to 500ms of audio) to the magnitudes matrix
         magnitudes.append(this_X)
         # And be sure that we store the correct label of this observation (0: music, 1: speech)
         labels.append(label)
     return magnitudes, labels
 
 
-# Store every magnitude frame and its label being music: 0 or speech: 1
+# Store every 500ms-magnitudes-group and its label being music: 0 or speech: 1
 Xs, ys = [], []
 audio_files = [music, speech]
 for label, files in enumerate(audio_files):
     for i in files:
-        magnitudes, labels = prepare_audio_file(i, label=label, fft_size=fft_size, hop_size=hop_size)
+        magnitudes, labels = prepare_audio_file(i,
+                                                label=label,
+                                                fft_size=fft_size,
+                                                hop_size=hop_size,
+                                                n_frames=n_frames,
+                                                frame_hops=frame_hops
+                                                )
         Xs += magnitudes
         ys += labels
 
-# Convert the magnitude frames and their corresponding labels to arrays:
 Xs = np.array(Xs)
 ys = np.array(ys)
 
-print(Xs.shape, ys.shape)
-
-# Just to make sure you've done it right.  If you've changed any of the
-# parameters of the dft/hop size, then this will fail.  If that's what you
-# wanted to do, then don't worry about this assertion.
-assert (Xs.shape == (15360, 43, 256, 1) and ys.shape == (15360,))
-
-# Let's plot the first magnitude matrix
+"""
+# Let's plot the first magnitude matrix (43 horizontal lines of magnitudes, each corresponding
+# to a 256-long frame. 43 frames, corresponds to 500ms). This is equivalent to 43 diagrams showing
+# 256 values of magnitude, only here we use colored lines to plot all 43 diagrams at once
 plt.imshow(Xs[0][..., 0])
 plt.title('label:{}'.format(ys[0]))
 plt.show()
+"""
 
 # The shape of our input to the network
 n_observations, n_height, n_width, n_channels = Xs.shape
 
-# Create a data set object from the data and the labels
+# Create a data set object from the data and the labels, to split the data into
+# training, validation and testing subsets
 ds = datasets.Dataset(Xs=Xs, ys=ys, split=[0.8, 0.1, 0.1], one_hot=True)
 
+"""
 Xs_i, ys_i = next(ds.train.next_batch())
 # Notice the shape this returns.  This will become the shape of our input and output of the network:
 print(Xs_i.shape, ys_i.shape)
@@ -138,13 +150,13 @@ plt.show()
 plt.imshow(Xs_i[1, :, :, 0])
 plt.title('label:{}'.format(ys_i[1]))
 plt.show()
+"""
 
 # ==== CREATING THE NETWORK ====
-tf.reset_default_graph()
-
+print('==== Creating the neural network... ====')
 # Create the input to the network. This is a 4-dimensional tensor
-# Don't forget that we should use None as a shape for the first dimension
-# Recall that we are using sliding windows of our magnitudes
+# Use None as a shape for the first dimension (for use with different batch sizes)
+# We are using sliding windows of our magnitudes as input
 X = tf.placeholder(name='X', shape=[None, n_height, n_width, n_channels], dtype=tf.float32)
 
 # Create the output to the network.  This is our one hot encoding of 2 possible values
@@ -156,23 +168,22 @@ n_filters = [9, 9, 9, 9]
 # Now let's loop over our n_filters and create the deep convolutional neural network
 H = X
 for layer_i, n_filters_i in enumerate(n_filters):
-    # Let's use the helper function to create our connection to the next layer:
+    # Create our connection to the next layer:
     H, W = utils.conv2d(H, n_filters_i, k_h=3, k_w=3, d_h=2, d_w=2, name=str(layer_i))
 
     # And use a non linearity
     H = tf.nn.relu(H)
 
-    # Just to check what's happening:
-    print(H.get_shape().as_list())
-
-# Connect the last convolutional layer to a fully connected network
+# Connect the last convolutional layer to a fully connected layer
 fc, _ = utils.linear(H, 100, name="fully_connected_layer_1")
 
 # And another fully connected layer, now with just 2 outputs, the number of outputs that our
 # one hot encoding has. A SoftMax activation is necessary to output a categorical probability distribution
 Y_predicted, _ = utils.linear(fc, 2, activation=tf.nn.softmax, name="fully_connected_layer_2")
 
+
 # ==== TRAINING THE NETWORK ====
+print('==== Training the network ====')
 # Cost function (measures the average loss of the batches)
 loss = utils.binary_cross_entropy(Y_predicted, Y)
 cost = tf.reduce_mean(tf.reduce_sum(loss, 1))
@@ -185,7 +196,7 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
 optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
 
-n_epochs = 10
+n_epochs = 15
 batch_size = 200
 
 sess = tf.Session()
@@ -195,14 +206,11 @@ sess.run(tf.global_variables_initializer())
 for epoch_i in range(n_epochs):
     print('==== EPOCH ', epoch_i, ' ====')
 
-    # Train
+    # Train with mini batches
     this_accuracy = 0
     iterations = 0
-
-    # Do our mini batches:
     for Xs_i, ys_i in ds.train.next_batch(batch_size):
-        # Note here: we are running the optimizer so
-        # that the network parameters train!
+        # Run the optimizer to train the network's parameters
         this_accuracy += sess.run([accuracy, optimizer], feed_dict={X: Xs_i, Y: ys_i})[0]
         iterations += 1
         print('Epoch:', epoch_i, ' Batch:', iterations - 1, ' Accuracy:', this_accuracy / iterations)
@@ -212,19 +220,19 @@ for epoch_i in range(n_epochs):
     this_accuracy = 0
     iterations = 0
     for Xs_i, ys_i in ds.valid.next_batch(batch_size):
-        # Note here: we are NOT running the optimizer!
-        # we only measure the accuracy!
+        # We are not running the optimizer here, only measuring the accuracy
         this_accuracy += sess.run(accuracy, feed_dict={X: Xs_i, Y: ys_i})
         iterations += 1
     print('Validation accuracy (data not used to train the network): ', this_accuracy / iterations)
 
-# Test : estimate the network's accuracy to generalize to unseen data after training
+# Test : estimate the network's accuracy when generalizing to unseen data after training
 this_accuracy = 0
 iterations = 0
 for Xs_i, ys_i in ds.test.next_batch(batch_size):
     this_accuracy += sess.run(accuracy, feed_dict={X: Xs_i, Y: ys_i})
     iterations += 1
 print('==== TEST ACCURACY (on unseen data, after training): ', this_accuracy / iterations, ' ====')
+
 
 # ==== INSPECTING THE NETWORK ====
 # Let's show the convolution filters (W tensors) learned by the network in each convolution layer
@@ -236,15 +244,27 @@ for layer_i in range(len(n_filters)):
     plt.title('Layer {}\'s Learned Convolution Kernels'.format(layer_i))
     plt.show()
 
+
 # ==== USING THE NETWORK ====
 # Input an unlabeled sound file and have the network classify it between music (0) or speech (1)
 
 
 def classify_unlabeled_audio_file(wav_file):
-    magnitudes, _ = prepare_audio_file(wav_file, fft_size=fft_size, hop_size=hop_size)
+    """
+    Takes an audio file as input, and uses the trained network
+    to classify it (either containing music: 0, or speech: 1)
+    :param wav_file: name of the .wav file to classify from the
+    gtzan_music_speech/music_speech/unlabeled_wav folder.
+    :return: the label that the network associates to the wav_file
+    """
+    wav_file = os.path.join(os.path.join(os.path.join(dst, 'music_speech'), 'unlabeled_wav'), wav_file)
+    magnitudes, _ = prepare_audio_file(wav_file,
+                                       fft_size=fft_size,
+                                       hop_size=hop_size,
+                                       n_frames=n_frames,
+                                       frame_hops=frame_hops
+                                       )
     return sess.run(tf.argmax(tf.reduce_mean(Y_predicted, 0), 0), feed_dict={X: magnitudes})
 
 
-unlabeled_dir = os.path.join(os.path.join(dst, 'music_speech'), 'unlabeled_wav')
-wav_file = os.path.join(unlabeled_dir, 'EricJohnson-CliffsOfDover.wav')
-print(classify_unlabeled_audio_file(wav_file))
+print(classify_unlabeled_audio_file('EricJohnson-CliffsOfDover.wav'))
--- libs/dft.py
+++ libs/dft.py
@@ -16,22 +16,44 @@ def ctoz(mag, phs):
 
 
 def dft_np(signal, hop_size=256, fft_size=512):
+    """
+    Splits the input signal into multiple frames (len(signal)/hop_size) of length fft_size
+    and applies the Discrete Fourier Transform to each of them. Returns the Real and Imaginary
+    components of the frequency domain for each frame.
+    :param signal: the signal to be processed
+    :param hop_size: defines the number of frames to extract from the input signal (if
+    hop_size is smaller than fft_size then most input points will belong to multiple frames.
+    If it is greater than fft_size, some points will not belong to any frame)
+    :param fft_size: the size of each frame to apply the DFT
+    :return: the real matrix contains stacked rows, each corresponding to the ReX(k) values
+    of an input frame (k from 0 to N/2). Row j corresponds to frame j. Similarly, the
+    imaginary matrix contains the ImX(k) values.
+    """
+    # Calculate the number of frames that we'll extract from the audio file
     n_hops = len(signal) // hop_size
     s = []
-    hann_win = hann(fft_size)
+    hann_window = hann(fft_size)
+    # Divide the signal into n_hops frames of length fft_size
     for hop_i in range(n_hops):
         frame = signal[(hop_i * hop_size):(hop_i * hop_size + fft_size)]
+        # Pad the last frame with 0s to make sure its length is equal to fft_size
         frame = np.pad(frame, (0, fft_size - len(frame)), 'constant')
-        frame *= hann_win
+        # Multiply the frame by the Hann window
+        frame *= hann_window
         s.append(frame)
     s = np.array(s)
+    # N is fft_size (the length of a frame)
     N = s.shape[-1]
-    k = np.reshape(np.linspace(0.0, 2 * np.pi / N * (N // 2), N // 2), [1, N // 2])
-    x = np.reshape(np.linspace(0.0, N - 1, N), [N, 1])
-    freqs = np.dot(x, k)
-    reals = np.dot(s, np.cos(freqs)) * (2.0 / N)
-    imags = np.dot(s, np.sin(freqs)) * (2.0 / N)
-    return reals, imags
+    # Create a vector containing all the values of k from 0 to N/2
+    k = np.reshape(np.linspace(0.0, (N // 2), N // 2), [1, N // 2])
+    # All the values of t (from 0 to N-1)
+    t = np.reshape(np.linspace(0.0, N - 1, N), [N, 1])
+    # All the 2.pi.k.t/N for all values of k and all values of t (matrix)
+    frequencies = np.dot(t, k * 2 * np.pi / N)
+    # Line number j of this matrix contains the ReX(k) values of the j-th frame of s
+    real = np.dot(s, np.cos(frequencies)) * (2.0 / N)
+    imaginary = np.dot(s, np.sin(frequencies)) * (2.0 / N)
+    return real, imaginary
 
 
 def idft_np(re, im, hop_size=256, fft_size=512):
